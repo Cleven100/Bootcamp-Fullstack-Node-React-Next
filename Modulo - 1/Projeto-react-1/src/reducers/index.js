9c532d73bdf66e591a69c518638a7932015c60d9
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-09 16:00:54 -0300

    09/04

--- Modulo - 1/Projeto-react-1/src/reducers/index.js
+++
@@ -1,4 +0,0 @@
-import { combineReducers } from 'redux';
-import { initial } from './initial'
-
-export default combineReducers({ initial });
--- Modulo - 1/Projeto-react-1/src/reducers/initial.js
+++
@@ -1,8 +0,0 @@
-export const initial = (state={},action) =>{
-  switch (action.type) {
-    case 'INIT':
-      return action;
-    default:
-      return state
-  }
-}
