90867ca03364041d10ec1eaf81ad8c5071d1d682
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-09 16:08:40 -0300

    09/04

--- Modulo - 1/Projeto-Calculadora-comJS/js/Controller/CalculadoraController.js
+++ Modulo - 1/Projeto-Calculadora-comJS/js/Controller/CalculadoraController.js
@@ -1,3 +1,7 @@
 class CalculadoraController {
-    
+    constructor() {
+        
+      this.dataElement = document.querySelector('.data').innerHTML = this.data;
+       this.hoursElement = document.querySelector('.hora').innerHTML = this.hora;
+    }
 }
