c889f4540e22e87f0e03e2c63d43c364ef2bb7aa
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-13 21:38:51 -0300

    13/04

---
+++ Modulo - 1/CRUDHOOKS/crud-simples/src/App.jsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD SIMPLES</h1>
+      <hr/>
+      <div className="row">
+        <div className="col-8">
+          Lista de tarefas
+        </div>
+         <div className="col-4">
+           Formulario
+         </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
