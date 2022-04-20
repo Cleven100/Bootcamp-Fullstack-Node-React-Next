229efbe8680851b63abab2e992f2c8e427ce4c40
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-19 21:53:30 -0300

    19/04

--- Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.js
+++ Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { Jumbotron, Container, Row, Col, Button, Form } from "react-bootstrap";
+
 import "./calculadora.css"
+
 
 
 
 export default function Calculadora() {
     return (
-        <h1>Oi sou a calculadora</h1>
+        <Jumbotron style={
+            {
+                background: 'transparent !important',
+                backgroundColor: '#007bff',
+
+                padding: '5px',
+                margin: '5px',
+                width: '400px'
+
+            }
+        }>
+            <Container>
+                <Row>
+                    <Col xs="3">
+                      <Button variant="danger">C</Button>
+                    </Col>
+
+                    <Col xs="9">
+                      <Form.Control type="text" name="txtValue" className="text-right" readOnly="readonly" />
+                    </Col>
+                </Row>
+            </Container>
+               
+        </Jumbotron>
+     
+        
     )
 }
