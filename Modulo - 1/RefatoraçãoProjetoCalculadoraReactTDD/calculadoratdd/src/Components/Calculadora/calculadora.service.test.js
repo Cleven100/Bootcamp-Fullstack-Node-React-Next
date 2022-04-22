db66d609a0e90f726ce897038571e62435d20e87
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-22 16:46:34 -0300

    22/04

--- Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.service.test.js
+++ Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.service.test.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { ReactDOM } from "react";
+import { Item } from "react-bootstrap/lib/Breadcrumb";
 import CalculadoraService from './calculadora.service';
 
 describe('Teste da CalculadoraService', () => {
    
     const [calcular] = CalculadoraService();
 
-    
+    it('deve Garantir que 1 + 4 = 5', () => {
+       let soma = calcular(1,4, '+');
+       expect(soma).toEqual(5);
+    })
 
 });
 
