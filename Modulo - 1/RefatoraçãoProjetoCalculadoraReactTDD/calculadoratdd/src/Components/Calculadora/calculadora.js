22881f8ad74c13fca55bb078fa37ba258570a3ac
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-22 14:50:11 -0300

    22/04

--- Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.js
+++ Modulo - 1/RefatoraçãoProjetoCalculadoraReactTDD/calculadoratdd/src/Components/Calculadora/calculadora.js
@@ -11,7 +11,7 @@ export default function Calculadora() {
         <Jumbotron style={
             {
                 background: 'transparent !important',
-                backgroundColor: '#007bff',
+                backgroundColor: '#68686b',
 
                 padding: '5px',
                 margin: '5px',
@@ -22,12 +22,76 @@ export default function Calculadora() {
             <Container>
                 <Row>
                     <Col xs="3">
-                      <Button variant="danger">C</Button>
+                      <Button variant="info">C</Button>
                     </Col>
 
                     <Col xs="9">
                       <Form.Control type="text" name="txtValue" className="text-right" readOnly="readonly" />
                     </Col>
+                </Row>
+
+                <Row>
+                   <Col>
+                       <Button variant="light">7</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">8</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">9</Button>
+                       
+                     </Col>
+                     <Col>
+                       <Button variant="light">/</Button>
+                     </Col>
+                </Row>
+
+                <Row>
+                   <Col>
+                       <Button variant="light">4</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">5</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">6</Button>
+                       
+                     </Col>
+                     <Col>
+                       <Button variant="light">x</Button>
+                     </Col>
+                </Row>
+
+                <Row>
+                   <Col>
+                       <Button variant="light">1</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">2</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">3</Button>
+                       
+                     </Col>
+                     <Col>
+                       <Button variant="light">+</Button>
+                     </Col>
+                </Row>
+
+                <Row>
+                <Col>
+                       <Button variant="light">0</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">.</Button>
+                     </Col>
+                     <Col xs={3}>
+                       <Button variant="info">=</Button>
+                     </Col>
+                     <Col>
+                       <Button variant="light">-</Button>
+                     </Col>
+                    
                 </Row>
             </Container>
                
