c509a944b3aa9a6fbf80e2d20d96c3616b1a50b5
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-23 20:57:04 -0300

    23/04

--- Modulo - 1/Projeto Conversor de moedas/conversormoedas/src/Components/ConversorMoedas/conversorMoedas.js
+++ Modulo - 1/Projeto Conversor de moedas/conversormoedas/src/Components/ConversorMoedas/conversorMoedas.js
@@ -9,7 +9,23 @@ export default function ConversorMoedas() {
          <div>
              <h1>Conversor de moedas</h1>
              <Jumbotron>
-               
+               <Form>
+                  <Form.row>
+                    <Col sm="3">
+                      <Form.Control
+                       placeholder="0"
+                       value={1} />                       
+                    </Col>
+
+                    <Col sm="3"></Col>
+
+                    <Col sm="1"></Col>
+
+                    <Col sm="3"></Col>
+                    
+                    <Col sm="2"></Col>
+                  </Form.row>
+               </Form>
              </Jumbotron>
          </div>
   )
