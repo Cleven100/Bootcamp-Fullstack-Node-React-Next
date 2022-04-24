bd94c918f6e0b5231f3b7bf279cfe34b919610db
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-23 21:33:20 -0300

    23/04

--- Modulo - 1/Projeto Conversor de moedas/conversormoedas/src/Components/ConversorMoedas/conversorMoedas.js
+++ Modulo - 1/Projeto Conversor de moedas/conversormoedas/src/Components/ConversorMoedas/conversorMoedas.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Jumbotron, Button, Form, Col, Spinner } from "react-bootstrap";
+import { Jumbotron, Button, Form, Col, Spinner, Alert } from "react-bootstrap";
 import { FontAwesomeIcon, fontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight } from '@fortawesome/free-solid-svg-icons';
 
 export default function ConversorMoedas() {
   return  (
          <div>
-             <h1>Conversor de moedas</h1>
-             <Jumbotron>
-               <Form>
-                  <Form.Row>
+             <h1 style={{margin: '10px'}}>Conversor de moedas</h1>
+             <Alert variant="danger" show={false} style={{margin: '10px'}}>
+               Erro obtendo dados de conversao , tente novamente.
+             </Alert>
+             <Jumbotron style={{margin: '10px'}}>
+               <Form >
+                  <Form.Row >
 
                     <Col sm="3">
                       <Form.Control
@@ -42,7 +45,7 @@ export default function ConversorMoedas() {
                     <Col sm="2">
                       <Button variant="dark" type="submit">
                         <Spinner animation="border" size="sm" style={{margin: "0 5px 0 0"}}/>
-
+                           
                         
                         Converter
                       </Button>
