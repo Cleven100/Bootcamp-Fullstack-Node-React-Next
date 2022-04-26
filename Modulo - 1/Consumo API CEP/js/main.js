382ea77d16883065aa48968b8e404c1bfc0523c4
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-04-26 16:42:13 -0300

    26/04

--- Modulo - 1/Consumo API CEP/js/main.js
+++ Modulo - 1/Consumo API CEP/js/main.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
-const pesquisarCep = () => {
-    alert("Hello world");
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('numero').value = endereco.complemento;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
 }
 
-document.getElementById('cep')
-        .addEventListener('focusout', pesquisarCep);
 
+const pesquisarCep = async () => {
+  const cep = document.getElementById("cep").value;
+  const url = `http://viacep.com.br/ws/${cep}/json`;
+
+  const dados = await fetch(url);
+
+  const endereco = await dados.json();
+  console.log(endereco);
+  preencherFormulario(endereco)
+};
+
+document.getElementById("cep").addEventListener("focusout", pesquisarCep);
