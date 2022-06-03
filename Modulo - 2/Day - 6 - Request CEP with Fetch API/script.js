1edf99da2b47bad4678ccd598dc074df386f6ec8
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-06-03 19:04:07 -0300

    03/06

---
+++ Modulo - 2/Day - 6 - Request CEP with Fetch API/script.js
@@ -0,0 +1,30 @@
+const cep = document.querySelector('#cep');
+
+
+cep.addEventListener('blur',(e) => {
+    let search = cep.value.replace('-', "");
+  
+    (async function  () {
+       let response = await fetch(`https://viacep.com.br/ws/${search}/json`);
+       let address = await response.json();
+       
+      showData(address);
+      
+      
+
+    })();
+    
+
+    
+
+} )
+
+
+const showData = (result) => {
+    for (const campo in result) {
+        if(document.querySelector("#" + campo)){
+            document.querySelector("#"+campo).value = result[campo]
+        }
+        
+    }
+}
