ee0bb877105a3d221cfad95ffdff7f88ee70f728
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-06-04 16:35:58 -0300

    04/06

--- Modulo - 2/Day - 9 - OmniStack - One/backend/src/controllers/PostController.js
+++ Modulo - 2/Day - 9 - OmniStack - One/backend/src/controllers/PostController.js
@@ -1,12 +1,36 @@
 const Post = require('../models/Post');
+const sharp = require('sharp');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async index(req, res) {
+    const posts = await Post.find().sort('-createdAt');
 
+    return res.json(posts);
+    
     },
 
     async store(req, res) {
-        
-     return res.json({ ok: true });
+       
+    const { author, place, description, hashtags } = req.body;
+    const { filename: image} = req.file;
+
+    await sharp(req.file.path)
+     .resize(500)
+     .jpeg({ quality: 70})
+     .toFile(
+         path.resolve(req.file.description, 'resized', image)
+     )
+
+    const post = await Post.create({
+        author,
+        place,
+        description,
+        hashtags,
+        image,
+    });
+
+     return res.json(post);
     }
 };
--- Modulo - 2/Day - 9 - OmniStack - One/backend/src/routes.js
+++ Modulo - 2/Day - 9 - OmniStack - One/backend/src/routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const multer = require('multer');
 const PostController = require('./controllers/PostController');
+const uploadConfig = require('./');
+const LikeController = require('./controllers/LikeController')
+
 
 const routes = new express.Router();
-const upload = multer();
+const upload = multer(uploadConfig);
 
 
 
-routes.post('/posts', PostController.store);
+routes.post('/posts', upload.single('image'), PostController.store);
+routes.get('/posts',  PostController.store);
+
+routes.post('/posts/:id/like',  LikeController.store);
 
 
 module.exports = routes;
