ba9835a89083d7dc1628c453ab6c191765157c7e
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-06-29 15:30:51 -0300

    29/06

---
+++ Modulo - 2/Day - 13 - Consuming API with AXIOS/front/main.js
@@ -0,0 +1,28 @@
+const url="http://localhost:5500/api";
+const newUser = {
+    name: "BLAK",
+    avatar: "",
+    city: "Rio do mel"
+}
+
+function getUser(){
+    axios.get(url)
+     .then(response => {
+        const data = response.data 
+        console.log(data);
+     })
+     .catch(error => console.log(error))
+}
+
+getUser()
+
+
+function addNewUser() {
+    axios.post(url, newUser)
+     .then(response => {
+        console.log(response.data)
+     })
+    .catch(error => console.log(error))
+}
+
+addNewUser()
