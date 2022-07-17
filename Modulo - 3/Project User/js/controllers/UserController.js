47630090525f71a09dc05a792c891fa95ccad435
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-17 10:22:43 -0300

    Twilio SMS

--- Modulo - 3/Project User/js/controllers/UserController.js
+++ Modulo - 3/Project User/js/controllers/UserController.js
@@ -8,19 +8,58 @@ class UserController {
       
     }
 
+    readPhoto(data) {
+        return new Promise((resolve, reject) => {
+             let fr = new FileReader();
+       fr.addEventListener('load', () => {
+        resolve(fr.result)
+         });
+         fr.addEventListener('error', (e)=>{
+            reject(e)
+         })
+       fr.readAsDataURL(data)
+    
+        })
+      
+}
     register() {
       let formEl = document.querySelector('.register');
       let elements = formEl.elements;
       let user;
+      let registerData = {};
+      [...elements].forEach((v) =>{
+        switch(v.type) {
+             case 'checkbox':
+                 registerData.admin = v.checked
+               break;
+             case 'file':   
+                this.readPhoto(v.files[0])
+               break;
+             default:
+                 registerData[v.name] = v.value
+               break;   
+        }
+      })
       if(JSON.stringify(this.users) == JSON.stringify({})) {
-        user = new User(0,elements.name.value, elements.photo.value, elements.email.value, elements.phone.value, elements.admin.value, elements.password.value );
+        user = new User(0,registerData.name,'', registerData.email, registerData.phone, registerData.admin, registerData.password );
       } else {
         let lastUser = Object.values({'a':'teste', 'b':'teste2'})[Object.values({'a':'teste','b':'teste2'}).length-1];
-        user = new User(0,lastUser.getId()+1, elements.photo.value, elements.email.value, elements.phone.value, elements.admin.value, elements.password.value );
+        user = new User(0,lastUser.getId()+1, registerData.name,' ', registerData.email, registerData.phone, registerData.admin, registerData.password );
       }
-      console.log(user)
+      let fileEl = elements.photo;
+      if(fileEl.files.length == 0){
+        user.setPhoto('img/icon.jpg')
+      }else {
+        this.readPhoto(fileEl.files[0]).then((result) => {
+       user.setPhoto(result)
+      }, (e)=>{
+        console.error(e)
+      })
+      console.log(user);
       
     }
+      }
+      
 
     addEventBtns() {
       document.querySelector('.add').addEventListener('click', ()=> {
@@ -42,6 +81,10 @@ class UserController {
       })
     }
 
+
+    setPhoto(photo){
+        this._photo = photo;
+    }
 }    
 
 
